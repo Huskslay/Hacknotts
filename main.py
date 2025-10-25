f65372b74e37e6c98d7b8dc3a7bcae53d631502c
Huskslay <75504464+Huskslay@users.noreply.github.com>
2025-10-25 17:40:05 +0100

    Added back generation

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
-from objects.object import Object
 
 pygame.init()
 delta = 0
 display = pygame.display.set_mode((1280,720))
 
 
+from objects.object import Object
 import objects.knight as knight
 import objects.enemy as enemy
+from generation.generation import Generation
 
 clock = pygame.time.Clock()
 
 objects: list[Object] = []
+generation = Generation()
+objects.append(generation)
 
 player = knight.Knight(display)
 slime1 = enemy.Slime(display)
