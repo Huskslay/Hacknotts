1cbfefa3980641d99f35be9f52c9e62bd0f94962
Axospace <epicjumper47@gmail.com>
2025-10-25 21:30:41 +0100

    Added a death screen

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ from objects.object import Object
 import objects.knight as knight
 import objects.enemy as enemy
 from generation.generation import Generation
+from objects.HealthBar import HealthBar
+from objects.DeathScreen import DeathScreen
 
 clock = pygame.time.Clock()
-
 objects: list[Object] = []
 generation = Generation()
+deathScreen = DeathScreen(display)
+player : knight.Knight
+
 objects.append(generation)
 
 player = knight.Knight(display)
 player.move_to(400, 400, generation.map)
+
+healthBar = HealthBar(display)
+healthBar.passPlayerReference(player)
+
 slime1 = enemy.Slime(display)
 slime1.move_to(400, 400, generation.map)
 objects.append(player)
@@ -26,6 +34,26 @@ for object in objects:
     if isinstance(object, enemy.Enemy):
         object.passPlayerReference(player)
 
+def recreateObjects():
+    global objects, player, healthBar
+    objects = []
+    objects.append(generation)
+
+    player = knight.Knight(display)
+    player.move_to(400, 400, generation.map)
+
+    healthBar = HealthBar(display)
+    healthBar.passPlayerReference(player)
+
+    slime1 = enemy.Slime(display)
+    slime1.move_to(400, 400, generation.map)
+    objects.append(player)
+    objects.append(slime1)
+    for object in objects:
+        if isinstance(object, enemy.Enemy):
+            object.passPlayerReference(player)
+    deathScreen.passPlayerReference(player)
+
 while True:
     delta = clock.tick(60)
 
@@ -33,13 +61,24 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
-        
-    display.fill("purple") 
     
-    for object in objects:
-        object.update(delta, generation.map, objects)
+    if player.currentHealth > 0:
+        
+        display.fill("purple") 
+        
+        for object in objects:
+            object.update(delta, generation.map, objects)
 
-    for object in objects:
-        object.draw(display)
+        for object in objects:
+            object.draw(display)
+        
+        healthBar.draw()
+    
+    else:
+        deathScreen.draw(delta)
+        if deathScreen.isRestarting():
+            recreateObjects()
+            deathScreen.restarting = False
 
     pygame.display.flip()
+
