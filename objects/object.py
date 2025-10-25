c4161cfa0b4d8dbcf88205eb4242235f5ee80ab4
Huskslay <75504464+Huskslay@users.noreply.github.com>
2025-10-25 18:48:43 +0100

    Sliding off walls

--- objects/object.py
+++ objects/object.py
@@ -2,7 +2,6 @@ import pygame
 
 from generation.map import Map
 
-from enum import Enum
 
 class Object:
     def update(self, delta: int, map: Map, objects: list["Object"]) -> None:
@@ -14,16 +13,24 @@ class Object:
     def move_by(self, x: float, y: float, map: Map) -> None:
         self.move_to(self.pos.x + x, self.pos.y + y, map)
 
-    def move_to(self, x: float, y: float, map: Map, force: bool = False) -> None:
+    def move_to(self, x: float, y: float, map: Map) -> None:
         old_pos = self.pos
         self.pos = pygame.Vector2(x, y)
-        self.hitbox.x = (int)(x + self.size[0] / 2 - self.hitboxSize[0] / 2)
-        self.hitbox.y = (int)(y + self.size[1] / 2 - self.hitboxSize[1] / 2)
-
-        if force: return
+        self.hitbox.x = (int)(self.pos.x + self.size[0] / 2 - self.hitboxSize[0] / 2)
+        self.hitbox.y = (int)(self.pos.y + self.size[1] / 2 - self.hitboxSize[1] / 2)
 
         if map.room.is_colliding(self.hitbox):
-            self.move_to(old_pos.x, old_pos.y, map, True)
+            self.pos = pygame.Vector2(x, old_pos.y)
+            self.hitbox.x = (int)(self.pos.x + self.size[0] / 2 - self.hitboxSize[0] / 2)
+            self.hitbox.y = (int)(self.pos.y + self.size[1] / 2 - self.hitboxSize[1] / 2)
+            if map.room.is_colliding(self.hitbox):
+                self.pos = pygame.Vector2(old_pos.x, y)
+                self.hitbox.x = (int)(self.pos.x + self.size[0] / 2 - self.hitboxSize[0] / 2)
+                self.hitbox.y = (int)(self.pos.y + self.size[1] / 2 - self.hitboxSize[1] / 2)
+                if map.room.is_colliding(self.hitbox):
+                    self.pos = old_pos
+                    self.hitbox.x = (int)(self.pos.x + self.size[0] / 2 - self.hitboxSize[0] / 2)
+                    self.hitbox.y = (int)(self.pos.y + self.size[1] / 2 - self.hitboxSize[1] / 2)
 
     def draw(self, display: pygame.Surface) -> None:
         pass 
